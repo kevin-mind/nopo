0b45febb8bfc4b4fc07fb97115d84e48f2f458fe
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-04-14 20:16:43 +0200

    Restructure to proper monorepo style

--- packages/docker/src/config.ts
+++ packages/docker/src/config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const root = path.join(__dirname, "..", "..");
+const root = path.join(__dirname, "..", "..", "..");
 
 export const config = {
   root,
