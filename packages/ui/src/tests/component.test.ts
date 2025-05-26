f4ca38cdf410e699ffbfffd51f92dfa87abcbc69
Kevin Meinhardt <kmeinhardt@mozilla.com>
2025-05-26 18:19:23 +0000

    Add stencil

--- packages/ui/src/tests/component.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from "vitest";
-import Component from "../component";
-
-describe("Component", () => {
-  it("runs a test", () => {
-    expect(Component()).toStrictEqual("Component");
-  });
-});
